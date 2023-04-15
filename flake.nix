2cd1b5db04d3647545ed5b40350d491446cdb0c7
Femi Agbabiaka <femi@femiagbabiaka.xyz>
2023-04-15 14:34:31 -0700

    start of emacs config

--- flake.nix
+++ flake.nix
@@ -1,11 +1,52 @@
 {
   description = "some config";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs = inputs @ { self, nixpkgs, nixos-hardware, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      username = "femi";
+      homeDirectory = "/home/${username}";
+    in
+    {
+      nixosConfigurations = {
+        laincomp = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./systems/laincomp
+            nixos-hardware.nixosModules.dell-xps-13-9380
+            ./configuration.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        femi = home-manager.lib.homeManagerConfiguration {
+
+          inherit pkgs;
+          modules = [
+            ./home/home.nix
+            {
+              home = {
+                inherit username homeDirectory;
+                stateVersion = "23.05";
+              };
+            }
+          ];
+        };
+      };
+    };
 }
