9ec1e1253aa74c24ae57a078f5c959c3fabf6422
Femi Agbabiaka <femi@femiagbabiaka.xyz>
2025-06-15 21:06:15 -0500

    Fix formatting and clean up system configuration
    - Reformat flake.nix for better readability
    - Remove unused packages from home-darwin-personal.nix
    - Add desktop environment config for jormungand system

--- flake.nix
+++ flake.nix
@@ -24,208 +24,212 @@
   };
 
   outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      nixos-hardware,
-      home-manager,
-      mkAlias,
-      nixos-wsl,
-      mac-app-util,
-      dagger,
-      emacs-overlay,
-      nix-darwin,
-      ...
-    }:
-    let
-      linux-pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
+  inputs@{
+    self,
+    nixpkgs,
+    nixos-hardware,
+    home-manager,
+    mkAlias,
+    nixos-wsl,
+    mac-app-util,
+    dagger,
+    emacs-overlay,
+    nix-darwin,
+    ...
+  }:
+  let
+  linux-pkgs = import nixpkgs {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+  };
 
-      darwin-pkgs = import nixpkgs {
+  darwin-pkgs = import nixpkgs {
+    system = "aarch64-darwin";
+    config.allowUnfree = true;
+    overlays = [
+      emacs-overlay.overlays.emacs
+    ];
+  };
+  in
+  {
+    darwinConfigurations = {
+      jormungand = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
-        config.allowUnfree = true;
-        overlays = [
-          emacs-overlay.overlays.emacs
-        ];
-      };
-    in
-    {
-      darwinConfigurations = {
-        jormungand = nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./systems/jormungand
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useUserPackages = true;
-              home-manager.users.femi = import ./home/home-darwin-personal.nix;
-              home-manager.extraSpecialArgs =
-                let
-                  username = "femi";
-                in
-                {
-                  inherit
-                    username
-                    self
-                    home-manager
-                    ;
-                  pkgs = darwin-pkgs;
-                };
-            }
-          ];
-          
-        };
-      };
-      nixosConfigurations = {
-        cassiopeia = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./systems/cassiopeia
-            ./systems/cassiopeia/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.femi = import ./home/home-linux.nix;
-              home-manager.extraSpecialArgs =
-                let
-                  username = "femi";
-                  homeDirectory = "/home/${username}";
-                in
-                {
-                  inherit
-                    username
-                    homeDirectory
-                    self
-                    home-manager
-                    ;
-                  pkgs = linux-pkgs;
-                };
-            }
-          ];
-        };
-        tachibana = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./systems/tachibana
-            ./systems/tachibana/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.femi = import ./home/home-linux.nix;
-              home-manager.extraSpecialArgs =
-                let
-                  username = "femi";
-                  homeDirectory = "/home/${username}";
-                in
-                {
-                  inherit
-                    username
-                    homeDirectory
-                    self
-                    home-manager
-                    ;
-                  pkgs = linux-pkgs;
-                };
-            }
-          ];
-        };
-        laincomp = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./systems/laincomp
-            ./systems/laincomp/configuration.nix
-            nixos-hardware.nixosModules.dell-xps-13-9380
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.femi = import ./home/home-linux.nix;
-              home-manager.extraSpecialArgs =
-                let
-                  username = "femi";
-                  homeDirectory = "/home/${username}";
-                in
-                {
-                  inherit
-                    username
-                    homeDirectory
-                    self
-                    home-manager
-                    ;
-                  pkgs = linux-pkgs;
-                };
-            }
-          ];
-        };
-        brain = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./systems/brain/configuration.nix
-            nixos-wsl.nixosModules.wsl
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.nixos = import ./home/home-linux.nix;
-              home-manager.extraSpecialArgs =
-                let
-                  username = "nixos";
-                  homeDirectory = "/home/${username}";
-                in
-                {
-                  inherit
-                    username
-                    homeDirectory
-                    self
-                    home-manager
-                    ;
-                  pkgs = linux-pkgs;
-                };
-            }
-          ];
-        };
-      };
-
-      homeConfigurations = {
-        worklaptop = home-manager.lib.homeManagerConfiguration {
-          pkgs = darwin-pkgs;
-          modules = [
-            ./home/home-darwin.nix
-            mac-app-util.homeManagerModules.default
-          ];
-          extraSpecialArgs =
+        specialArgs = { inherit inputs; };
+        modules = [
+          mac-app-util.darwinModules.default
+          ./systems/jormungand
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useUserPackages = true;
+            home-manager.users.femi = import ./home/home-darwin-personal.nix;
+            home-manager.sharedModules = [
+              mac-app-util.homeManagerModules.default
+            ];
+            home-manager.extraSpecialArgs =
             let
-              username = "fagbabiaka";
-              homeDirectory = "/Users/${username}";
+            username = "femi";
             in
             {
               inherit
-                username
-                homeDirectory
-                inputs
-                dagger
-                ;
+              username
+              self
+              home-manager
+              ;
               pkgs = darwin-pkgs;
-              system = "aarch64-darwin";
             };
+          }
+        ];
+
+      };
+    };
+    nixosConfigurations = {
+      cassiopeia = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./systems/cassiopeia
+          ./systems/cassiopeia/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.femi = import ./home/home-linux.nix;
+            home-manager.extraSpecialArgs =
+            let
+            username = "femi";
+            homeDirectory = "/home/${username}";
+            in
+            {
+              inherit
+              username
+              homeDirectory
+              self
+              home-manager
+              ;
+              pkgs = linux-pkgs;
+            };
+          }
+        ];
+      };
+      tachibana = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./systems/tachibana
+          ./systems/tachibana/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.femi = import ./home/home-linux.nix;
+            home-manager.extraSpecialArgs =
+            let
+            username = "femi";
+            homeDirectory = "/home/${username}";
+            in
+            {
+              inherit
+              username
+              homeDirectory
+              self
+              home-manager
+              ;
+              pkgs = linux-pkgs;
+            };
+          }
+        ];
+      };
+      laincomp = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./systems/laincomp
+          ./systems/laincomp/configuration.nix
+          nixos-hardware.nixosModules.dell-xps-13-9380
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.femi = import ./home/home-linux.nix;
+            home-manager.extraSpecialArgs =
+            let
+            username = "femi";
+            homeDirectory = "/home/${username}";
+            in
+            {
+              inherit
+              username
+              homeDirectory
+              self
+              home-manager
+              ;
+              pkgs = linux-pkgs;
+            };
+          }
+        ];
+      };
+      brain = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./systems/brain/configuration.nix
+          nixos-wsl.nixosModules.wsl
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixos = import ./home/home-linux.nix;
+            home-manager.extraSpecialArgs =
+            let
+            username = "nixos";
+            homeDirectory = "/home/${username}";
+            in
+            {
+              inherit
+              username
+              homeDirectory
+              self
+              home-manager
+              ;
+              pkgs = linux-pkgs;
+            };
+          }
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      worklaptop = home-manager.lib.homeManagerConfiguration {
+        pkgs = darwin-pkgs;
+        modules = [
+          ./home/home-darwin.nix
+          mac-app-util.homeManagerModules.default
+        ];
+        extraSpecialArgs =
+        let
+        username = "fagbabiaka";
+        homeDirectory = "/Users/${username}";
+        in
+        {
+          inherit
+          username
+          homeDirectory
+          inputs
+          dagger
+          ;
+          pkgs = darwin-pkgs;
+          system = "aarch64-darwin";
         };
       };
-
-      formatter."x86_64-linux" = linux-pkgs.nixfmt-rfc-style;
-      formatter."x86_64-darwin" = darwin-pkgs.nixfmt-rfc-style;
-      formatter."aarch64-darwin" = darwin-pkgs.nixfmt-rfc-style;
-
-      devShells."x86_64-linux".default = with linux-pkgs; mkShell { packages = [ nixfmt ]; };
-
-      devShells."aarch64-darwin".default = with darwin-pkgs; mkShell { packages = [ nixfmt ]; };
     };
+
+    formatter."x86_64-linux" = linux-pkgs.nixfmt-rfc-style;
+    formatter."x86_64-darwin" = darwin-pkgs.nixfmt-rfc-style;
+    formatter."aarch64-darwin" = darwin-pkgs.nixfmt-rfc-style;
+
+    devShells."x86_64-linux".default = with linux-pkgs; mkShell { packages = [ nixfmt ]; };
+
+    devShells."aarch64-darwin".default = with darwin-pkgs; mkShell { packages = [ nixfmt ]; };
+  };
 }
--- home/home-darwin-personal.nix
+++ home/home-darwin-personal.nix
@@ -1,12 +1,4 @@
-
-{
-  pkgs,
-  lib,
-  username,
-  dagger,
-  ...
-}:
-{
+{ pkgs, username, ... }: {
   imports = [
     ./apps/vscode
     ./apps/fish
@@ -24,94 +16,88 @@
 
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
-  home.packages =
-    with pkgs;
-    [
-      aider-chat-with-playwright
-      ansible
-      ansible-language-server
-      automake
-      awscli2
-      bat
-      cmake
-      lima
-      colima
-      coreutils
-      crane
-      curl
-      docker
-      docker-compose
-      delve
-      dive
-      dockerfile-language-server-nodejs
-      fastly
-      fd
-      fq
-      gh
-      ghostty
-      go
-      golangci-lint
-      golangci-lint-langserver
-      gopls
-      gotools
-      git-crypt
-      graphviz
-      helm-ls
-      helix
-      jq
-      k9s
-      kakoune
-      kubectx
-      kubernetes-helm
-      lldb
-      nerd-fonts.fira-code
-      nil
-      nixfmt
-      nodejs
-      nushell
-      platinum-searcher
-      rbenv
-      ripgrep
-      roswell
-      rubyPackages.solargraph
-      rustup
-      shellcheck
-      spotify
-      stern
-      terraform-docs
-      terraform-ls
-      tflint
-      tfswitch
-      tilt
-      tmux
-      tmux-xpanes
-      vault
-      vscode-langservers-extracted
-      yaml-language-server
-      zellij
-      zig
-      zls
-      zoxide
-      zstd
-      git
-      _1password-gui-beta
-      neofetch
-      htop
-      discord
-      fish
-      wget
-      light
-      fishPlugins.done
-      fishPlugins.fzf-fish
-      fishPlugins.forgit
-      fishPlugins.hydro
-      fzf
-      nushell
-      tailscale
-      zoxide
-      delta
-      bat
-    ];
+  home.packages = with pkgs; [
+    aider-chat-with-playwright
+    ansible
+    ansible-language-server
+    automake
+    awscli2
+    bat
+    cmake
+    lima
+    colima
+    coreutils
+    crane
+    curl
+    docker
+    docker-compose
+    delve
+    dive
+    dockerfile-language-server-nodejs
+    fastly
+    fd
+    fq
+    gh
+    go
+    golangci-lint
+    golangci-lint-langserver
+    gopls
+    gotools
+    git-crypt
+    graphviz
+    helm-ls
+    helix
+    jq
+    k9s
+    kakoune
+    kubectx
+    kubernetes-helm
+    lldb
+    nerd-fonts.fira-code
+    nil
+    nixfmt
+    nodejs
+    nushell
+    platinum-searcher
+    rbenv
+    ripgrep
+    roswell
+    rubyPackages.solargraph
+    rustup
+    shellcheck
+    spotify
+    stern
+    terraform-docs
+    terraform-ls
+    tflint
+    tfswitch
+    tilt
+    tmux
+    tmux-xpanes
+    vault
+    vscode-langservers-extracted
+    yaml-language-server
+    zellij
+    zig
+    zls
+    zoxide
+    zstd
+    git
+    neofetch
+    htop
+    discord
+    fish
+    wget
+    fishPlugins.done
+    fishPlugins.forgit
+    fishPlugins.hydro
+    fishPlugins.fzf
+    fzf
+    nushell
+    zoxide
+    delta
+    bat
+  ];
 
   fonts.fontconfig.enable = true;
 }
