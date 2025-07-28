19a835c5d360e23760110e4d0cf1703412609d22
Femi Agbabiaka <femi@femiagbabiaka.xyz>
2025-07-28 15:22:57 -0500

    update (#15)

--- home/apps/neovim/default.nix
+++ home/apps/neovim/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  neovim-nightly-overlay,
   ...
 }:
 let
@@ -8,42 +9,46 @@ let
   nvimConfig = pkgs.writeText "init.lua" initLua;
   neovimWrapper = pkgs.symlinkJoin {
     name = "neovim-for-femi";
-    paths = [ pkgs.neovim ];
+    paths = [ neovim-nightly-overlay.packages.${pkgs.system}.default ];
     buildInputs = [ pkgs.makeWrapper ];
 
     postBuild = ''
-      # Wrap neovim binary
-      wrapProgram $out/bin/nvim \
-         --set NVIM_APPNAME "neovim-for-femi" \
-         --add-flags "-u ${nvimConfig}" \
-	 --prefix PATH : "${lib.makeBinPath [
-	    pkgs.checkmake
-	    pkgs.clippy
-	    pkgs.codespell
-	    pkgs.eslint_d
-	    pkgs.gawk
-	    pkgs.gofumpt
-	    pkgs.goimports-reviser
-	    pkgs.golangci-lint
-	    pkgs.gotools
-	    pkgs.jq
-	    pkgs.libxml2
-	    pkgs.lua51Packages.lua
-	    pkgs.lua51Packages.luarocks
-	    pkgs.mdformat
-	    pkgs.nodePackages.js-beautify
-	    pkgs.python313Packages.demjson3
-	    pkgs.rubyPackages.htmlbeautifier
-	    pkgs.rustfmt
-	    pkgs.shellcheck
-	    pkgs.stylua
-	    pkgs.taplo
-	    pkgs.tflint
-	    pkgs.trivy
-	    pkgs.yq
-	    pkgs.zig
-        pkgs.yamllint
-	 ]}"
+            # Wrap neovim binary
+            wrapProgram $out/bin/nvim \
+               --set NVIM_APPNAME "neovim-for-femi" \
+               --add-flags "-u ${nvimConfig}" \
+      	 --prefix PATH : "${
+          lib.makeBinPath [
+            pkgs.checkmake
+            pkgs.clippy
+            pkgs.codespell
+            pkgs.eslint_d
+            pkgs.gawk
+            pkgs.gofumpt
+            pkgs.goimports-reviser
+            pkgs.golangci-lint
+            pkgs.gotools
+            pkgs.gopls
+            pkgs.jq
+            pkgs.libxml2
+            pkgs.lua51Packages.lua
+            pkgs.lua51Packages.luarocks
+            pkgs.lua-language-server
+            pkgs.mdformat
+            pkgs.nodePackages.js-beautify
+            pkgs.python313Packages.demjson3
+            pkgs.rubyPackages.htmlbeautifier
+            pkgs.rustfmt
+            pkgs.shellcheck
+            pkgs.stylua
+            pkgs.taplo
+            pkgs.tflint
+            pkgs.trivy
+            pkgs.yq
+            pkgs.zig
+            pkgs.yamllint
+          ]
+        }"
     '';
   };
 in
