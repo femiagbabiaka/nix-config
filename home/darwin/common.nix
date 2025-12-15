b264b08395c11154a0f0af8be13dd75abff02e0f
Femi Agbabiaka <femi@femiagbabiaka.xyz>
2025-12-15 09:40:43 -0600

    update

--- home/darwin/common.nix
+++ home/darwin/common.nix
@@ -1,12 +1,13 @@
-{ pkgs
-, username
-, lib ? pkgs.lib
-, extraImports ? [ ]
-, extraPackages ? [ ]
-, homeStateVersion ? "23.05"
-, homeExtraConfig ? { }
-, configOverrides ? { }
-, ...
+{
+  pkgs,
+  username,
+  lib ? pkgs.lib,
+  extraImports ? [ ],
+  extraPackages ? [ ],
+  homeStateVersion ? "23.05",
+  homeExtraConfig ? { },
+  configOverrides ? { },
+  ...
 }:
 let
   inherit (lib) unique;
@@ -22,160 +23,148 @@ let
     ../apps/kakoune
   ];
 
-  basePackages =
-    with pkgs;
-    [
-      kanata
-      ansible
-      automake
-      awscli2
-      bat
-      broot
-      cmake
-      claude-code
-      codex
-      coreutils
-      crane
-      curl
-      delta
-      delve
-      dive
-      docker
-      docker-compose
-      elan
-      fd
-      fish
-      fishPlugins.done
-      fishPlugins.hydro
-      fontconfig
-      fq
-      fzf
-      gh
-      git
-      git-crypt
-      go
-      golangci-lint
-      golangci-lint-langserver
-      gopls
-      gotools
-      graphviz
-      haskell.compiler.ghc910
-      haskell-language-server
-      helix
-      helm-ls
-      htop
-      jq
-      jujutsu
-      k9s
-      kakoune-lsp
-      kubernetes-helm
-      lldb
-      mosh
-      neofetch
-      nerd-fonts.fira-code
-      nil
-      nixfmt-rfc-style
-      nodejs
-      nushell
-      platinum-searcher
-      radicle-node
-      radicle-tui
-      rbenv
-      ripgrep
-      rustup
-      shellcheck
-      stern
-      terraform-docs
-      terraform-ls
-      tflint
-      tfswitch
-      tilt
-      vscode-langservers-extracted
-      wget
-      yaml-language-server
-      zig
-      zls
-      zoxide
-      zstd
+  basePackages = with pkgs; [
+    ansible
+    automake
+    awscli2
+    bat
+    broot
+    claude-code
+    cmake
+    codex
+    coreutils
+    crane
+    curl
+    delta
+    delve
+    dive
+    docker
+    docker-compose
+    elan
+    fd
+    fish
+    fishPlugins.done
+    fishPlugins.hydro
+    fontconfig
+    fq
+    fzf
+    gh
+    git
+    git-crypt
+    go
+    golangci-lint
+    golangci-lint-langserver
+    gopls
+    gotools
+    graphviz
+    haskell-language-server
+    haskell.compiler.ghc910
+    helix
+    helm-ls
+    htop
+    jq
+    jujutsu
+    k9s
+    kakoune-lsp
+    kubernetes-helm
+    lldb
+    mosh
+    neofetch
+    nerd-fonts.fira-code
+    nil
+    nixfmt-rfc-style
+    nodejs
+    nushell
+    platinum-searcher
+    radicle-node
+    radicle-tui
+    rbenv
+    ripgrep
+    rustup
+    shellcheck
+    stern
+    terraform-docs
+    terraform-ls
+    tflint
+    tfswitch
+    tilt
+    vscode-langservers-extracted
+    wget
+    yaml-language-server
+    zig
+    zls
+    zoxide
+    zstd
+    _1password-gui
     aider-chat-with-playwright
     ansible
     automake
     awscli2
     bat
+    bat
     cmake
     colima
     coreutils
     curl
+    delta
     dockerfile-language-server
     fastly
     fd
     fq
+    fzf
+    gh
+    ghc
     go
     golangci-lint
     golangci-lint-langserver
-    google-cloud-sdk
-    gh
-    gotools
     gopls
-    ghc
-    ghostty
+    gotools
     haskell-language-server
     helix
-    mpv
-    stack
-    kubernetes-helm
-    jujutsu
     jq
+    jujutsu
     k9s
-    kubectx
     kakoune-lsp
-    mitschemeX11
+    kubectx
+    kubernetes-helm
     mosh
+    mpv
+    nerd-fonts.fira-code
     nil
     nixfmt-rfc-style
     nushell
     platinum-searcher
     pulseaudio
     rbenv
-    rustup
     ripgrep
+    rustup
     shellcheck
-    spotify
+    stack
     stern
     tailscale
-    toolbox
-    _1password-gui
-    yubioath-flutter
     terraform-docs
-    tflint
     terraform-ls
+    tflint
     tfswitch
-    tmux
-    tmux-xpanes
     vscode-langservers-extracted
     yaml-language-server
-    zig
-    zls
-    zstd
-    zoxide
     zathura
     zellij
-    nerd-fonts.fira-code
-    fzf
-    delta
-    bat
-    ];
+    zig
+    zls
+    zoxide
+    zstd
+  ];
 in
 {
   imports = sharedImports ++ extraImports;
 
-  home =
-    {
-      inherit username;
-      stateVersion = homeStateVersion;
-      packages = unique (basePackages ++ extraPackages);
-    }
-    // homeExtraConfig;
+  home = {
+    inherit username;
+    stateVersion = homeStateVersion;
+    packages = unique (basePackages ++ extraPackages);
+  }
+  // homeExtraConfig;
 
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
